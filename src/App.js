9fe26a4285e7a62631c7e638b0528baff92f3a05
inekeyuliam <inekemargareta20@gmail.com>
2022-12-20 06:36:41 +0700

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css'
+import Sidebar from './components/Sidebar/Sidebar';
+import Content from './components/Content/Content';
+import RightSide from './components/RightSide/RightSide';
 
 function App() {
   return (
     <div className="App">
-        <div className="AppGlass">
-          
+        <div className="AppGlass"> 
+          <Sidebar/>
+          <Content/>
+          <RightSide/>
         </div>
     </div>
   );
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../../img/logo.png'
+import './Sidebar.css'
+const Sidebar = () => {
+  return (
+	<div className="Sidebar">
+		{/* logo */}
+		<div className="logo">
+			<img src="" alt="" />
+			<span></span>
+		</div>
+	</div>
+  )
+}
+
+export default Sidebar
