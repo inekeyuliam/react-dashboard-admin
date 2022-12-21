ca171e4bae9fd290afc78f1213290d342bd7c427
inekeyuliam <inekemargareta20@gmail.com>
2022-12-21 20:44:13 +0700

    expanded card and chart css

--- src/components/CardData/CardData.jsx
+++ src/components/CardData/CardData.jsx
@@ -105,8 +105,12 @@ function ExpandedCard({param, setExpanded}){
 		}}
 		layoutId='expandableCard'
 		>
-		<div>
-				<UilTimes onClick={setExpanded} />
+		<div style={{
+				alignSelf:'flex-end',
+				cursor: 'pointer',
+				color: 'white'
+			}}>
+			<UilTimes onClick={setExpanded}  />
 		</div>
 		<span>{param.title}</span>
 		<div className="chart-container">
