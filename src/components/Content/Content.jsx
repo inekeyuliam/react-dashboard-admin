12db9e689195ab1e7c29b90bec072647c6a5af1a
inekeyuliam <inekemargareta20@gmail.com>
2022-12-23 21:50:42 +0700

    recent orders

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from '../Card/Card'
+import Table from '../Table/Table'
 import './Content.css'
 const Content = () => {
   return (
     <div className="main-dashboard">
         <h1>Dashboard</h1>
         <Card/>
+        <Table/>
     </div>
   )
 }
